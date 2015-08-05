2d00fffcf8bf551c8cae4dabef4eca2c276c03da
Garrard Kitchen <garrard.kitchen@qualtrak.com>
2015-08-05 15:56:52 +0100

    Changed copy and author to Qualtrak

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'dataconnectorapi'
-copyright = '2015, Garrard Kitchen'
-author = 'Garrard Kitchen'
+copyright = '2015, Qualtrak Solutions Ltd'
+author = 'Qualtrak'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
